8c7f15b3a86be30e7cc7cb224e3aa3a84ada6e0a
unnamedKnight <backfrontdev47@gmail.com>
2023-05-31 20:29:21 +0600

    Add Review Model

--- src/projects/models.py
+++ src/projects/models.py
@@ -10,10 +10,29 @@ class Project(models.Model):
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_code = models.CharField(max_length=2000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, unique=True, editable=False
+    )
 
     def __str__(self) -> str:
         return self.title
 
 
+# text choices value field in Review Model
+class VoteTypes(models.TextChoices):
+    UP_VOTE = ("up", "Up vote")
+    DOWN_VOTE = ("down", "Down vote")
 
+
+class Review(models.Model):
+    # review_owner = models.ForeignKey()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=50, choices=VoteTypes.choices)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, unique=True, editable=False
+    )
+
+    def __str__(self) -> str:
+        return self.value
