96e186a2111798b4ee468b56e05335b59928136f
unnamedKnight <backfrontdev47@gmail.com>
2023-06-02 20:32:49 +0600

    Add form styling

--- src/projects/forms.py
+++ src/projects/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-
+from django import forms
 from .models import Project
 
 
@@ -14,3 +14,12 @@ class ProjectForm(ModelForm):
             "tags",
             "source_code",
         )
+
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input"})
