c69404f406cf0be02cf4abaa76ccbdfe5dc9a0b7
unnamedKnight <backfrontdev47@gmail.com>
2023-06-07 16:14:24 +0600

    Add profile_detail page

--- src/user_profiles/urls.py
+++ src/user_profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profiles", views.profiles, name="profiles")
+    path("profiles", views.profiles, name="profiles"),
+    path("profile/<str:pk>", views.profile_detail, name="profile_detail")
 ]
--- src/user_profiles/views.py
+++ src/user_profiles/views.py
@@ -10,7 +10,15 @@ def profiles(request):
     context = {"profiles": profiles}
     return render(request, "user_profiles/profiles.html", context)
 
+
 def profile_detail(request, pk):
     profile = Profile.objects.get(pk=pk)
-    context = {"profile": profile}
+    top_skills = Profile.objects.exclude(skill__description__iexact="")
+    other_skills = Profile.objects.filter(skill__description="")
+    context = {
+        "profile": profile,
+        top_skills: "top_skills",
+        other_skills: "other_skills",
+    }
+
     return render(request, "user_profiles/profile_detail.html", context)
