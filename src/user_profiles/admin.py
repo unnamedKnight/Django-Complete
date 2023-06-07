adc9a1aed10f79725cc99b1b9ca9430cfbae074e
unnamedKnight <backfrontdev47@gmail.com>
2023-06-07 15:09:53 +0600

    Add Skill model

--- src/user_profiles/admin.py
+++ src/user_profiles/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Profile
+from .models import Profile, Skill
+
 
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Skill)
--- src/user_profiles/views.py
+++ src/user_profiles/views.py
@@ -9,3 +9,8 @@ def profiles(request):
     profiles = Profile.objects.all()
     context = {"profiles": profiles}
     return render(request, "user_profiles/profiles.html", context)
+
+def profile_detail(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    context = {"profile": profile}
+    return render(request, "user_profiles/profile_detail.html", context)
